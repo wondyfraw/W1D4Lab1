9435c6d776b15745ecc9db2426104ba677bc0aca
wondyfraw <wondyfraw@users.noreply.github.com>
2020-04-25 05:46:28 +0200

    add javascript console output display code

---
+++ js/lab5.js
@@ -0,0 +1,11 @@
+
+function onClickMe(){
+	
+	let email = document.getElementById("emailInput").value;
+	let password = document.getElementById("passwordInput").value;
+	let url = document.getElementById("urlInput").value;
+
+	console.log("Email =" + email +"\n" +
+	            "Password = " + password + "\n" +
+				"URL=" + url);
+}
